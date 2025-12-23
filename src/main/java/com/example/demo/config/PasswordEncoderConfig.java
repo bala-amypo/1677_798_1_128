d8172c0173db87a8b930a18051cfeaf0117642cf
bala-amypo <bala@amypo.in>
2025-12-23 05:13:59 +0000

    User commit

--- src/main/java/com/example/demo/config/PasswordEncoderConfig.java
+++ src/main/java/com/example/demo/config/PasswordEncoderConfig.java
@@ -1,4 +1,4 @@
-package com.example.demo.config;
+package com.example.demo;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -6,7 +6,7 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
 @Configuration
-public class SecurityConfig {
+public class PasswordEncoderConfig {
     
     @Bean
     public PasswordEncoder passwordEncoder() {
