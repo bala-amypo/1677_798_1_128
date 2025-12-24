65a786d9adb2dc7ab28ce9e9b71a020ced713edf
bala-amypo <bala@amypo.in>
2025-12-24 06:23:21 +0000

    User commit

--- src/main/java/com/example/demo/controller/RebalancingAlertRecordController.java
+++ src/main/java/com/example/demo/controller/RebalancingAlertRecordController.java
@@ -1,7 +1,7 @@
 package com.example.demo.controller;
 
 import com.example.demo.entity.RebalancingAlertRecord;
-import com.example.demo.service.RebalancingAlertRecordService;
+import com.example.demo.service.RebalancingAlertService;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.web.bind.annotation.*;
 
