310af322c94e6c59821554ac846fd09bb882eb06
bala-amypo <bala@amypo.in>
2025-12-25 02:48:59 +0000

    User commit

--- src/main/java/com/example/demo/config/JwtTokenProvider.java
+++
@@ -1,9 +0,0 @@
-package com.example.demo.config;
-
-import org.springframework.stereotype.Component;
-
-@Component
-public class JwtTokenProvider {
-    // This class can be used for additional JWT token management
-    // Currently handled by JwtUtil
-}
